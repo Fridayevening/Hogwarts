d14625fb1197610abe5103e08c3cf28a0b1fd0c0
zhangming <qianzm2490@gmail.com>
2023-04-24 18:49:44 +0800

    feat: reset css by normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import 'normalize.css';
+
 import router from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
